8e09ba6324fa336f93a196fc5cca12e4da8bc12c
Aotae <nathan.ao.pang@gmail.com>
2023-05-05 13:02:15 -0700

    door lerping finished
    door lerping finished

--- Class Clown/Assets/Scripts/Openable.cs
+++ Class Clown/Assets/Scripts/Openable.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class Openable : MonoBehaviour
 {
     [SerializeField] private bool isopen = false;
+    public Vector3 targetangle;
+    private Vector3 startangle;
+    // funny mutex lock but not really
+    private bool opening = false;
     public void open()
     {
+        startangle = transform.eulerAngles;
+        targetangle = startangle;
         if (!isopen)
         {
-
-            transform.RotateAround(new Vector3(transform.position.x, transform.position.y, transform.position.z - .75f), transform.up, 90f);
-            isopen = true;
+            targetangle.y = startangle.y + 90;
+            if (!opening)
+            {
+                StartCoroutine(LerpDoor(1f, startangle.y, targetangle.y));
+                opening = true;
+                isopen = true;
+            }
         }
         else
         {
-            transform.RotateAround(new Vector3(transform.position.x - .75f, transform.position.y, transform.position.z), transform.up, -90f);
-            isopen = false;
+            targetangle.y = startangle.y - 90;
+            if (!opening)
+            {
+                StartCoroutine(LerpDoor(1f, startangle.y, targetangle.y));
+                opening = true;
+                isopen = false;
+            }
         }
     }
-
+    IEnumerator LerpDoor(float duration, float startyangle,float targetyangle)
+    {
+        float time = 0;
+        opening = true;
+        while(time < duration)
+        {
+            float yangle = Mathf.Lerp(startyangle,targetyangle,(time/duration));
+            time += Time.deltaTime;
+            Vector3 temp = new Vector3(transform.rotation.x, yangle, transform.rotation.z);
+            transform.eulerAngles = temp;
+            yield return null;
+        }
+        transform.eulerAngles = new Vector3(transform.rotation.x, targetyangle, transform.rotation.z);
+        opening = false;
+        yield return null;
+    }
 }
