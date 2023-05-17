9351ab7928ec862adcfd3e8efca3a2698c151b33
Aotae <nathan.ao.pang@gmail.com>
2023-05-16 23:40:41 -0700

    changed interact

--- Class Clown/Assets/Scripts/Interact.cs
+++ Class Clown/Assets/Scripts/Interact.cs
@@ -9,8 +9,12 @@ public class Interact : MonoBehaviour
     [SerializeField] private LayerMask pickupable;
     [SerializeField] private float pickupdist = 2f;
     [SerializeField] private Transform grabpoint;
+<<<<<<< Updated upstream
     [SerializeField] private AudioSource pickupSound;
     [SerializeField] private AudioSource throwSound;
+=======
+    private Transform highlight;
+>>>>>>> Stashed changes
 
     private bool holding = false;
     GameObject held;
@@ -18,8 +22,11 @@ public class Interact : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        // raycast from player every frame its only one though so should be computationally trivial
+        // outline selectable objects and allows user to interact with them
+        if (highlight != null)
         {
+<<<<<<< Updated upstream
             if (Physics.Raycast(playerCam.position, playerCam.forward, out RaycastHit hit, pickupdist, pickupable) && !holding){
                 Debug.Log(hit.transform.name);
                 if (hit.transform.TryGetComponent(out PickUP PickUP)) {
@@ -30,15 +37,54 @@ public class Interact : MonoBehaviour
                     Debug.Log("Grabbable");
                 }else if(hit.transform.TryGetComponent(out Openable openable)){
                     openable.open();
+=======
+            highlight.gameObject.GetComponent<Outline>().enabled = false;
+            highlight = null;
+        }
+        if (Physics.Raycast(playerCam.position, playerCam.forward, out RaycastHit selectable, pickupdist, pickupable))
+        {
+            highlight = selectable.transform;
+            Debug.Log(highlight.name);
+            if (highlight.CompareTag("Selectable"))
+            {
+                if (highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.red;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (holding && held != null)
+                    {
+                        holding = false;
+                        held.GetComponent<PickUP>().Drop();
+                        held = null;
+                    }
+                    if (highlight.TryGetComponent(out PickUP PickUP) && !holding)
+                    {
+                        PickUP.Grab(grabpoint, gameObject);
+                        held = PickUP.gameObject;
+                        holding = true;
+                        Debug.Log("Grabbable");
+                    }
+                    else if (highlight.TryGetComponent(out Openable openable))
+                    {
+                        openable.open();
+                    }
+>>>>>>> Stashed changes
                 }
             }
-            else if(holding && held != null){
-                holding = false;
-                held.GetComponent<PickUP>().Drop();
-                held = null;
+            else
+            {
+                highlight = null;
             }
         }
-
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if(holding && held != null)
