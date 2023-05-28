3e54059eca2b36d6ea85451ee62a3002a52f849c
Aotae <nathan.ao.pang@gmail.com>
2023-05-27 21:31:35 -0700

    detection revamp and gym scene revamp

--- Class Clown/Assets/Scripts/Detectors/Detection.cs
+++ Class Clown/Assets/Scripts/Detectors/Detection.cs
@@ -1,75 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
 
 public class Detection : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    // followed this guide 
+    // https://github.com/Comp3interactive/FieldOfView
+    // and edited to draw fov cone
+    public MeshRenderer Detector;
+    public int MatNum;
+    public Material detected;
+    public Material fovmat;
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+    private Material undetected;
+    private PlayerInputs PlayerInput;
+    private Material[] matlist;
+
+    private void Start()
     {
-        if (other.gameObject.name == "Player")
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        undetected = Detector.materials[MatNum];
+        PlayerInput = playerRef.GetComponent<PlayerInputs>();
+        StartCoroutine(FOVRoutine());
+    }
+    private void Update()
+    {
+        matlist = Detector.materials;
+        if (canSeePlayer)
         {
-            Debug.Log("Caught");
+            matlist[MatNum] = detected;
+            Detector.materials = matlist;
+        }
+        else
+        {
+            matlist[MatNum] = undetected;
+            Detector.materials = matlist;
+        }
+
+    }
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+            if (PlayerInput.crouch)
+            {
+                DrawFOV();
+            }
+            else
+            {
+                // clear fov
+            }
         }
     }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.name == "Player")
-        {
-            Debug.Log("Caught");
 
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        // checks if player is in the list of collisions on the target layermask from a overlap sphere
+        if (rangeChecks.Length != 0)
+        {
+            // first one it sees
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
         }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+
+    }
+    private void DrawFOV()
+    {
+
     }
 }
-/*
- * Prank List <Do any three of these to unlock escape conditions>
- * 
- * Steal Teacher's lunch -> Teacher's lounge -> Keep lunch on you to the exit
- * Pop all the basketballs -> Gym
- * Make a Half court shot -> Gym
- * Change your report card -> Principal's office
- * Start a food fight -> Cafeteria
- * Make the Teacher open the windows
- * Pull Fire Alarm -> Escape out front lobby
- * Mess with the thermostat
- * Erase escaping footage
- * Collect all the chewed erasers (Collect Quest -> Bonus)
- * 
- */
-
-/*
- * Escape Conditions
- *
- * Release a Class Pet
- * Escape out the Gym (Do the Gym pranks)
- * Escape out the Front Lobby (Do one of each, Cafeteria, Classroom, Gym)
- * Escape out the Classroom Window (Release the class pet, Get Teacher to open the window, )
- *
- */
-
-/*
- * Once you escape you get a score for how well you did
- * 
- * Silent: How much you were noticed
- * Time taken: Lower the better
- * Casualties: none prank targets harmed = deduction of score
- * 
- */
-
-/*
- * 
- * Priority of Assets 
- * Room assets/Hallways Assets 1
- * Animation 
- * 
- */
-
-/* Game Loop of stealth
- * 
- * Caught
- * teacher walks at you
- * teacher collides with you 
- * reset scene
- * A*
- * Else Make sure not to get Caught
- * 
- */
