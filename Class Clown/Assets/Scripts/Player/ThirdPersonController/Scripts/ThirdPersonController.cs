39cc3b22c454d8f1450a071aeb8cb61906864385
Aotae <nathan.ao.pang@gmail.com>
2023-05-30 16:39:23 -0700

    Deleted old scripts

--- Class Clown/Assets/Scripts/Player/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Class Clown/Assets/Scripts/Player/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -135,14 +135,7 @@ namespace Player
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<PlayerInputs>();
-#if ENABLE_INPUT_SYSTEM 
             _playerInput = GetComponent<PlayerInput>();
-#else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
-#endif
-
-
-
             _fallTimeoutDelta = FallTimeout;
         }
 
